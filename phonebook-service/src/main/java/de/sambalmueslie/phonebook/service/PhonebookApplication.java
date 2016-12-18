aefaac08ae74d8922879e5bb1f9ab923b62ff77e
Oliver Eckle <me@sambalmueslie.de>
2016-12-18 01:04:34 +0100

    initial setup

---
+++ phonebook-service/src/main/java/de/sambalmueslie/phonebook/service/PhonebookApplication.java
@@ -0,0 +1,14 @@
+package de.sambalmueslie.phonebook.service;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+public class PhonebookApplication extends Application<PhonebookConfiguration> {
+
+	@Override
+	public void run(PhonebookConfiguration configuration, Environment environment) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ phonebook-service/src/main/java/de/sambalmueslie/phonebook/service/PhonebookConfiguration.java
@@ -0,0 +1,7 @@
+package de.sambalmueslie.phonebook.service;
+
+import io.dropwizard.Configuration;
+
+public class PhonebookConfiguration extends Configuration {
+
+}
